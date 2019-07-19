589825c70d125e86d5fe8bf20990999d51361032
ALEX Solutions <alexsolutions@annette-ong.local>
2019-07-19 15:33:58 +1000

    Created basic required JS files for burger

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
 import App from '../../App';
+import classes from './Layout.module.css';
 
 const layout = ( props ) => (
     <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
+    <main className={classes.Content}>
         {/* output the component this layout wraps */}
         {props.children}
     </main>
