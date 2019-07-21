a0a1008c0e06d8e72d03392ac5618afa377acb0e
ALEX Solutions <alexsolutions@annette-ong.local>
2019-07-22 09:52:17 +1000

    Show price dynamically change

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -11,6 +11,8 @@ const controls = [
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
+        {/* displaying price dynamically */}
+        <p>Current Price: <strong>${props.price.toFixed(2)}</strong></p>
         {/* create array of buildControl objects */}
         {controls.map(ctrl =>
             <BuildControl 
