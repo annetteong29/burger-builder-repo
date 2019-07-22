7c63f11c71889ef19073a5b878c086a111bda6bd
ALEX Solutions <alexsolutions@annette-ong.local>
2019-07-23 09:53:32 +1000

    Set state dynamically + retrieving backend data

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -21,16 +21,22 @@ class BurgerBuilder extends Component {
     //     super(props);
     //     this.state = {}
     state = {
-        ingredients: {
-            salad: 0,
-            bacon: 0,
-            cheese: 0,
-            meat: 0
-        },
+        ingredients: null,
         totalPrice: 4,
         purchasable: false,
         purchasing: false,
-        loading: false
+        loading: false,
+        error: false
+    }
+
+    componentDidMount() {
+        axios.get('https://react-burger-builder-76c41.firebaseio.com/ingredients.json')
+            .then(response => {
+                this.setState({ingredients: response.data})
+            })
+            .catch(error => {
+                this.setState({error: true})
+            });
     }
 
     purchaseHandler = () => {
@@ -123,11 +129,33 @@ class BurgerBuilder extends Component {
         }
         // {salad: true, meat: false, ...}
 
-        let orderSummary = <OrderSummary 
+        // since state is dynamically rendered from backend when page is loaded,
+        // burger will not immediately appear, need to show loading until
+        // data is loaded from backend
+        let orderSummary = null;
+        let burger = this.state.error ? 
+            <p>Ingredients can't be loaded!</p> : <Spinner />
+
+        if (this.state.ingredients) {
+            burger = (
+                <Aux>
+                    <Burger ingredients={this.state.ingredients}/>
+                    <BuildControls 
+                            ingredientAdded={this.addIngredientHandler}
+                            ingredientRemoved={this.removeIngredientHandler}
+                            disabled={disabledInfo}
+                            price={this.state.totalPrice}
+                            purchasable={this.state.purchasable}
+                            ordered={this.purchaseHandler}
+                        />
+                </Aux>
+            );
+            orderSummary = <OrderSummary 
             ingredients={this.state.ingredients}
             purchaseCancelled={this.purchaseCancelHandler}
             purchaseContinued={this.purchaseContinueHandler}
-            price={this.state.totalPrice}/>
+            price={this.state.totalPrice}/>;
+        }
         if (this.state.loading) {
             orderSummary = <Spinner />;
         }
@@ -139,15 +167,7 @@ class BurgerBuilder extends Component {
                     modalClosed={this.purchaseCancelHandler}>
                     {orderSummary}
                 </Modal>
-                <Burger ingredients={this.state.ingredients}/>
-                <BuildControls 
-                    ingredientAdded={this.addIngredientHandler}
-                    ingredientRemoved={this.removeIngredientHandler}
-                    disabled={disabledInfo}
-                    price={this.state.totalPrice}
-                    purchasable={this.state.purchasable}
-                    ordered={this.purchaseHandler}
-                />
+                {burger}
             </Aux>
         );
     };
--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -4,13 +4,22 @@ import Aux from '../Aux/Aux';
 
 const withErrorHandler = (WrappedComponent, axios) => {
     return class extends Component {
+        // we want to register interceptors 
+        // BEFORE child components are rendered
+
         state = {
             error: null
         }
 
-        componentDidMount() {
+        // only called after all child elements have been rendered
+        // i.e. end of return statement, in this case WrappedComponent
+        // BUT since that reaches out to web in componentDidMount of WrappedComponent,
+        // interceptors that return error msgs are not set up
+        // unless componentWillMount or constructor is used
+        componentWillMount() {
             axios.interceptors.request.use(req => {
-                // clear any errors to ensure no error when HTTP request is sent
+                // clear any errors to ensure no error when 
+                // new HTTP request is sent
                 this.setState({error: null});
                 return req;
             })
