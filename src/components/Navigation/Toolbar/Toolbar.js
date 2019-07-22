ccd85a70fc4b244e0a01b7d2e4061e99c725d4fa
ALEX Solutions <alexsolutions@annette-ong.local>
2019-07-22 12:25:57 +1000

    More responsive adjustments due to side drawer

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -10,7 +10,7 @@ const toolbar = (props) => (
             {/* height="80%" */}
             <Logo />
         </div>
-        <nav>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems />
         </nav>
     </header>
